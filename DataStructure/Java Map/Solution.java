64b65cff025ac24c1244627c4300bdb23f9e564d
ssnitish <ssnitish0000@gmail.com>
2017-05-26 13:58:39 +0530

    Create Solution.java
    implement phone-book using HashMap and then queries

---
+++ DataStructure/Java Map/Solution.java
@@ -0,0 +1,41 @@
+// 
+
+import java.util.*;
+import java.io.*;
+class Solution {
+    public static void main(String [] args) throws Exception {
+        
+        /* Read input and save as entries in a HashMap */
+        
+        //BufferedReader is used instead of Scanner since it's faster  and throws Exception is must when using this
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.valueOf(br.readLine());
+        HashMap<String, Integer> map = new HashMap<>();
+        while (n-- > 0) {
+            String name = br.readLine();
+            int phone   = Integer.valueOf(br.readLine());
+            map.put(name, phone);
+        }
+        
+        /*  to print map (key value ) using for loop
+        for(Map.Entry m:map.entrySet()){
+            System.out.println(m.getKey() + " " +  m.getValue());
+        }*/
+        
+        
+        
+        /* Read each query and check if its in our HashMap */
+        String s;
+        while((s = br.readLine()) != null) {
+            if (map.containsKey(s)) {
+                System.out.println(s + "=" + map.get(s));
+            } else {
+                System.out.println("Not found");
+            }
+        }
+   
+
+        br.close();
+    }
+}
