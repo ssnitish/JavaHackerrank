cc223f522798badb0d95fb2aed603990539c7eba
ssnitish <ssnitish0000@gmail.com>
2017-05-27 16:39:15 +0530

    Update Solution.java
    Stack, balanced brackets.

--- DataStructure/Java Stack/Solution.java
+++ DataStructure/Java Stack/Solution.java
@@ -16,14 +16,11 @@ class Solution{
            if(s.charAt(i) == '(' || s.charAt(i) == '{' || s.charAt(i) == '['){
                stack.push(s.charAt(i));
            }
-           else if(s.charAt(i) == ')' || s.charAt(i) == '}' || s.charAt(i) == ']'){
-               if(stack.empty() || stack.peek()==s.charAt(i)){
-                   return false;
-               }
-               else 
-                   stack.pop();
-           }
-           
+            else if(s.charAt(i)==')' && !stack.empty() && stack.peek()=='(') stack.pop();
+            else if(s.charAt(i)==']' && !stack.empty() && stack.peek()=='[') stack.pop();
+            else if(s.charAt(i)=='}' && !stack.empty() && stack.peek()=='{') stack.pop();
+          
+            else return false;
        }
        return stack.empty() ? true : false ; // lastly stack should be empty.
    } 
